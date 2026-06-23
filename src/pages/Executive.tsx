9911c4992079069b82ab260a56e806497590afb0
Pawan Rao <pawanraojarvis942@gmail.com>
2026-06-23 20:45:55 +0400

    update website

--- src/pages/Executive.tsx
+++ src/pages/Executive.tsx
@@ -13,7 +13,7 @@ const Executive = () => {
   return (
     <div className="p-10">
       <h1 className="text-4xl font-bold mb-8">
-        Executive
+        Executive & VIP
       </h1>
 
       <div className="grid md:grid-cols-2 gap-6">
