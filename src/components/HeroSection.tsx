c3420fdb53bd3c76de858ea0be9d3202671d2e11
Pawan Rao <pawanraojarvis942@gmail.com>
2026-06-24 11:21:40 +0400

    adjusted hero video opacity

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -47,7 +47,7 @@ const HeroSection = () => {
   <source src={heroVideo} type="video/mp4" />
 </video>
 
-<div className="absolute inset-0 bg-black/50"></div>
+<div className="absolute inset-0 bg-black/25"></div>
 
       {/* Animated floating orbs */}
       <FloatingOrb
